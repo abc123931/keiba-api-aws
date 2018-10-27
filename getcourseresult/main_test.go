00a41508ce72cdbd32ce871726e4cf9782e8d90e
abc123931 <abc123931@gmail.com>
2018-10-27 09:57:02 +0900

    getcourseresultの単体テストを作成

--- getcourseresult/main_test.go
+++ getcourseresult/main_test.go
@@ -8,23 +8,23 @@ import (
 )
 
 func TestHandler(t *testing.T) {
-	t.Run("getHorseName正常終了", func(t *testing.T) {
-		res := getHorseName(&FakeTable{}, events.APIGatewayProxyRequest{Body: `{"id": "test_id"}`})
+	t.Run("getCourseResult正常終了", func(t *testing.T) {
+		res := getCourseResult(&FakeTable{}, events.APIGatewayProxyRequest{Body: `{"id": "test_id"}`})
 		if res.Error != "" {
-			t.Fatal("getHorseName failed: ", res.Error)
+			t.Fatal("getCourseResult failed: ", res.Error)
 		}
 
-		horse := Horse{ID: "test_id", Name: "test_name"}
-		expected := &Response{Data: horse, Error: ""}
+		courseResult := CourseResult{ID: "test_id"}
+		expected := &Response{Data: courseResult, Error: ""}
 		if !reflect.DeepEqual(*res, *expected) {
 			t.Fatalf("response not same expected: expected is %v response is  %v", *expected, *res)
 		}
 	})
 
-	t.Run("getHorseName異常終了", func(t *testing.T) {
-		res := getHorseName(&FakeTable{}, events.APIGatewayProxyRequest{Body: `{"id": ""}`})
+	t.Run("getCourseResult異常終了", func(t *testing.T) {
+		res := getCourseResult(&FakeTable{}, events.APIGatewayProxyRequest{Body: `{"id": ""}`})
 		if res.Error == "" {
-			t.Fatal("getHorseName not failed")
+			t.Fatal("getCourseResult not failed")
 		}
 
 		expected := "Key: 'Request.ID' Error:Field validation for 'ID' failed on the 'required' tag"
@@ -35,10 +35,10 @@ func TestHandler(t *testing.T) {
 	})
 
 	t.Run("createResponse正常終了", func(t *testing.T) {
-		horse := Horse{ID: "test_id", Name: "test_name"}
-		response := &Response{Data: horse}
+		courseResult := CourseResult{ID: "test_id", SapporoTurf: "(0-1-0-0)"}
+		response := &Response{Data: courseResult}
 		resBody := createResponse(response)
-		expected := `{"data":{"id":"test_id","name":"test_name"},"error":""}`
+		expected := `{"data":{"id":"test_id","sapporo_turf":"(0-1-0-0)","hakodate_turf":"","fukushima_turf":"","nigata_turf":"","tokyo_turf":"","nakayama_turf":"","tyukyo_turf":"","kyoto_turf":"","hanshin_turf":"","kokura_turf":"","1000_turf":"","1200_turf":"","1400_turf":"","1600_turf":"","1800_turf":"","2000_turf":"","2200_turf":"","2400_turf":"","2500_turf":"","3000_turf":"","3200_turf":"","3600_turf":""},"error":""}`
 
 		if resBody != expected {
 			t.Fatalf("response not same expected: expected is %v response is  %v", expected, resBody)
@@ -48,7 +48,7 @@ func TestHandler(t *testing.T) {
 	t.Run("createResponseErrorがある場合", func(t *testing.T) {
 		response := &Response{Error: "errorだよ"}
 		resBody := createResponse(response)
-		expected := `{"data":{"id":"","name":""},"error":"errorだよ"}`
+		expected := `{"data":{"id":"","sapporo_turf":"","hakodate_turf":"","fukushima_turf":"","nigata_turf":"","tokyo_turf":"","nakayama_turf":"","tyukyo_turf":"","kyoto_turf":"","hanshin_turf":"","kokura_turf":"","1000_turf":"","1200_turf":"","1400_turf":"","1600_turf":"","1800_turf":"","2000_turf":"","2200_turf":"","2400_turf":"","2500_turf":"","3000_turf":"","3200_turf":"","3600_turf":""},"error":"errorだよ"}`
 
 		if resBody != expected {
 			t.Fatalf("response not same expected: expected is %v response is  %v", expected, resBody)
